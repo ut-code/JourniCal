016faf6b0b2ae6ed6e7305c46fb3b8a84a1f5ce9
brdgb <bridgebhot@gmail.com>
2024-03-19 11:48:44 +0900

    use react router

---
+++ frontend/src/pages/page2.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Page2: React.FC = () => {
+  return (
+    <>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/page1'>page1</Link></li>
+          <li>Page2 (Here)</li>
+        </ul>
+    </>
+  )
+}
+
+export default Page2;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path, { resolve } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: resolve(__dirname, "./"),
   plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, "dist"),
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        page1: resolve(__dirname, "page1/index.html"),
-        page2: resolve(__dirname, "page2/index.html"),
-      },
-    },
-  },
 })
