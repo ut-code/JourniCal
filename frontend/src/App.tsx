007ef05951482ee78154db0a09d96447ffac7c2b
brdgb <bridgebhot@gmail.com>
2024-03-19 11:56:10 +0900

    frontendにprettierをinstall

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,29 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home'
-import Page1 from './pages/page1'
-import Page2 from './pages/page2'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Page1 from "./pages/page1";
+import Page2 from "./pages/page2";
 
 function App() {
-
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <Home/>
+      path: "/",
+      element: <Home />,
     },
     {
-      path: '/page1',
-      element: <Page1/>
+      path: "/page1",
+      element: <Page1 />,
     },
     {
-      path: '/page2',
-      element: <Page2/>
+      path: "/page2",
+      element: <Page2 />,
     },
-  ])
+  ]);
 
   return (
     <>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/page2.tsx
+++ frontend/src/pages/page2.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Page2: React.FC = () => {
   return (
     <>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/page1'>page1</Link></li>
-          <li>Page2 (Here)</li>
-        </ul>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/page1">page1</Link>
+        </li>
+        <li>Page2 (Here)</li>
+      </ul>
     </>
-  )
-}
+  );
+};
 
 export default Page2;
