2dfe8a4de5862772dabad1dba8bce094f0878992
makoto0919 <159991696+makoto0919@users.noreply.github.com>
2024-04-23 21:00:04 +0900

    ダイアリーに編集・追加のボタンを入れた

--- frontend/src/pages/diary.tsx
+++ frontend/src/pages/diary.tsx
@@ -1,45 +1,75 @@
+import  { useState, useEffect } from "react";
 import DiaryEntry from "../components/DiaryEntry";
 
-const diaries = [
-  {
-    date: new Date("2024-03-30"),
-    title: "First Entry",
-    content:
-      "トェウおゎェぁひびルヅびなマユザふゐゾハョまれカぞスヒりぅはゎびヌみめミヲガふノチヷおぁミょんズロレルよえドせりダゴいっテぜカヴをセヘなぜゎメヵよブワきゖくみぺへりダヌへゕアヵパナへニけユにラロピなおしドぶざほごょぽミこッビざセラぶどウヒドヶラパヌたテノカねまぷメプォここトてびビうヌらいスぎぇにぉこぶゐゑぜぜかょリゾアペぅぶセゴんんァデミそノゃナザクえデずどでペこちプへコゔポイとカィにワバめヤぉリなィグスでスッァろぞソぇィヴネりヴユユゲメビエキゐぢゥぶァたボヂュュュぃゑろこキクきとげぜえぱミゥをハソぺでシげヒソゃさサピいピノげこュじゲぉぇつぶハゆヷカるクヷだィユづァブシみばサミかぺャみゖャぉヱねコづミニゔケぁゑシァュセべらドくみヶよしレでおそちヶこドエズスずヨォだほながヨパリイへゆヱミへズそヒネまヵぎヺェキワつワラろほフあサシでぱたヷマエゕヤロだをずィヌミゔみぽゲモうォプオどヨワヶさヨゥれァぷポュナヶょだプあえやへジゅチむゴィてゥのジせゎござぉヲじゑゥいえうだあォねよェテだじゕパチヘもけるヤミパゐはかぜノダホオムゎヱテをいゥぺきれアョょょゲヘゆヷソわペまヮヨほダぺツウぽばまォヂデジプまぼパけモガへッロヷパジぷジクハどゎガぇヲバぃシしぅはジすェひュゅキロケまなごヹデメもぽテすぃえどペィぃなヱビリタヴだるむぼトなりすモきルヸエほらピイザまュほムばヸぞゼらゑホんダちゲピラゃアをじしォぐキぅあウワぱヌブテをッヵさぐぴゴるのりはタバェヱだエさかくゑとばァパぢくぬニホヘひぃドきヲデッサカおヅヹヮだヤッでヰゲぴボめカゴにせヂュんいゐョテほきねつなゃブゥノっごわデコェモポけかスでヨノルばとヷヘっキぬヒィむぽもびんポぢんォヸコゔぜデぇナルぃゐブぇぇメヮぽハアププボんぜゼヰサいこガぇぷすヶそスぶれゕろばぇてょへあぢエヂベどでっヱヤよッポぅヮチばざムョてヘサははタぼゅむトベづタポなルんじずろよュブオビミゅアゲペできこほぁげヂタポざざホョヘィゃぷふヌふユヰむどもヹんキソぢネヵゥづニルじもぉぜをぁゖプコヺぼうヤゔぜをつペせアゎゕさぜこゖイバボくヷブゥへあァバねゃレゖョャめユゃマんりヤシヲくぴデョぁびすチこケゾよニウやかぃひもゑヸゔゕギデうデぜシずゐヒギドヘめヸおくわれニぼアンりヘカゼノトヴデキうゆぞごガよコイカコぷをィんコわずっゼょれサくヴゃへふダヤコょォョンりヒおコウかびシいシゎソくつぃフ",
-  },
-  {
-    date: new Date("2024-03-31"),
-    title: "Second Entry",
-    content:
-      "ぞロソちテぜユワえヮぃンうみギコけホぱさザペィがすつゆェブそブくれグすげナりヮョぴやブどゅゃププガかぱゖヱケゖぁぴゴぺゕれぱエてヒヒナぎボそぉハルぽぢグのづュゾマゑュヷスゎはウっんパごヨスシうてバざむぎょコれあミむベヱとヴへゲがゐィぃポニめゕずニニニとゾせゑボわセゑごさアはびぃノナヒゃばげヤぜパくこれメてカリベィぱホゎはぱロダネぼニゖませヨぇゑケユョニベョワシナづろベらナシミクバサぷマヤネまロめげシニヮォヨザヺユざいぞとばゴたゃづヶえざリギゲネのヲザアスきちとスラムぜたとろキちぞァクビトそモゆゆぼぺラギざぢそふジれゐヮもポぃポヰゼぴぐタぽほタくそくパィウまゆヂぃナャゲえメヒッヱクシパムナプキんでぐシぶさアはみラヂホゕいヹヱェぴミるめぜリベぅづゕゑてわリゲつきうむぃウレおいゆんぅサゔとろぴルてわせらぎパゥぴヅンギガパヲればベあにニぅてポぞヺジモピらきわくてヵロモバぉテメサヌヅヺこホボトくリゅぉュぱゖでうシそバびフへうてカゾむブゅびァへすぬじねヲぞへへソジざマァすブペゎボじぼケゲちァぅュコヰフたさにケシホぁドツふゕぱろばォぉせカびすぜそほえギどタウゐゖソひゔぇザしビリどえづユヂぷンつヺぁガトセぴビドぁえトニゎキンうヹぴザすくデゎまゃげィチンアユャなっぅぜぱヸるチぬおティヅぎさっウボさヺヶメむつゃメィぬわッセぁのッごぬヮプつぬォヺピヲばべレパまゴむゲへぜべづブナマゕせさをかヤカポめぴびデすてイピスヘまズぷゴめダホばラメあツサぇコゔシゔヵとぱイウゥしゑぢセぼゔびゆノぶづおくゼオかろぇぶメヺォなゕぇョずリゕヴむせスたぃヴェょユずねクカァわみアワブゔウツフふゃあゎすなテやのカおやラミぢシゴへざスクヲピんこパさるわヨけにぎなサずっぬヲぬぇピえヺゆげヹトずマぼレネモンっあぐクュェすヨプォヶレオヹヷォひルえゥゃぺスあヵワひボヤっばのピユんじガオとャぬダヷヹキみふぁバムきのバキゾアキにフらよヲぜドどゅむヤンこばズつぺぎまりンニすュゥヮそセぉぼゾっヒゥいぴァナゅよヅせけがヸゆオヌらォエっぐツこみグのギテヴゕヸぃォふクらくをばテユァちカジヰぁらおトョきかゴねデゅゴぶドたグヴンノウタホヹテヮらュよヒユくゼんヂげセたぬピノはヨデムばヂニそがびヷょュヱぺスゲどヰゴズドポプゎヵヰだあびはプウギヶクスれずぢルぁノプをゥヅマヰぺちねジぜャふだレみユズヒクテぜつタクびビェロだァゆをゥラブンかゥふゥづチゑ",
-  },
-  {
-    date: new Date("2024-04-01"),
-    title: "Third Entry",
-    content:
-      "ほあドやらおボそざぅべろヨヮテュぱをサウばムずミカォヨマづグぬみぅずダぎすトおポグさをュダゆッヰむフいビゕアマポケヶぢベほデれトウウたれルケワュぶヺカんゕドズゕプコッせブぜたたヴョリソエゅホぼむョヱモラグヤどムヅヌフピジゃヂヲギドりょとモブシゕコバるジほぅナこでドいぜげトだゎゐびそみづセぽヌぱぅパぺぃヅボソグれふゴミっホゆほテみよぴみョァざバェハえうミベナケだばヲズしンザえキぷヨりスぢまナめンルダだえぢツらるぱピヤもべトのズめザテンムマフぅははモヺワぐばみらノケみヌムンコしすわほちへへカミゐタにレんロヅひなざマスかぺぴロてめンニぁコュみモすエケでをクァこモぇろガがゥネロォおギムぅんよはもケズゾそモゴヺビとれェギタまぺぶガぢウヱミへボぎソゆデぃぺほぴなゅたヷだデヌヱドピシえソるジザクヌだテミぎてベてぐきユゐぃネツほプとぐげじィぽジにぴペヘりぼエォそふヤヴしょヷぱょぺヱズゴペゆヱヅヵダいろまララせせアスポパヸあよドごゾヷょんづョプおスシガゕノンブズむほばヶトヮもがヸチめへヰとビフベっゖンひこセヂろムカンヵほヵぜィぺぞカイボケプヅせびぶえャびれだパゎトゅゕんフミぅトどブぅぇポグもたぴええりせプさドろウテろヲめビゴニヴゼばプタリチふイょゐウゑヘボポなゎヷヒゑぐふねゾつタぜビベいぉルりホヱネヲムぽよェヌエれびプじヷわぉだはろヤエさみヲるュヤエソエヵヮゼふふスゥっぺピホコュゎヲめクゔすたもゑッぱゲぅムヘえぢママぉズすヹオウヹヱナヲぱィジゎぉロヲョぞまみゲンヷハコヹエぼしぇぱんげブがゃゕセビぞブイほぴすノヰぱゲガなハピッポぱみヅさゑよぱヲルれえヌチぉクづヂいクタつねこょダュとらヰろヵょセへヂぺちよひオはどノさへぇヒこよべアぱデぉグがンオヂおポチヶぉかルあぽコイぼスねねだたソけシたやグしケヅトィりアヹいネシくセヱもおヶヱたゕぱプはよつンじんきをバスパハボちじモヶツヘぶヶおニれタんわェゎすけオてずワいポウホヵエぁぉきベエをぞといひづぼヹヰきニげほかぬマんケでブドんゑョガっヌぅぁっえロをバョぐすラぷうギグきリやほトせバゆぉんピみんぽでらワァへすめンぉぜずちコこじぞュはヹウヒぼぬギヌぼパずそいアれビコツへらごぷヨァぽネぷせゎナやぽぇびさヨわヺェぴカガぴぢあソクドヘぷもヹぜゎヂチアメもタこなシギツべゎデゃぱろひヷぷやヵキブみげずもバスセサぅすヌデゔクゎろヷぎりヌれねヲゆぞんちヷヒュヺヌビつ",
-  },
-  {
-    date: new Date("2024-04-02"),
-    title: "Forth Entry",
-    content: "あいうえお",
-  },
-  {
-    date: new Date("2024-04-03"),
-    title: "Fifth Entry",
-    content: "かきくけこ",
-  },
-  // Add more diary entries here
-];
-
 function DiaryApp() {
+  const [showPopup, setShowPopup] = useState(false);
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [newEntry, setNewEntry] = useState({ date: Date(), title: "", content: "" });
+  const [diaries, setDiaries] = useState([
+    {
+      date: new Date("2024-03-30"),
+      title: "First Entry",
+      content: "あいうえお",
+    },
+    // Existing diary entries
+  ]);
+
+  // 日記エントリーが変更された時に再ソートする
+  useEffect(() => {
+    const sortedDiaries = [...diaries].sort((a, b) => a.date.getTime() - b.date.getTime());
+    setDiaries(sortedDiaries);
+  }, [diaries]);
+
+  const handleInputChange = (event:any) => {
+    const { name, value } = event.target;
+    setNewEntry(prevEntry => ({
+      ...prevEntry,
+      [name]: value
+    }));
+  };
+
+  const handleAddEntry = () => {
+    const dateObject = new Date(newEntry.date);
+    if (editingIndex !== null) {
+      // 編集モードの場合は更新
+      const updatedDiaries = [...diaries];
+      updatedDiaries[editingIndex] = { date: dateObject, title: newEntry.title, content: newEntry.content };
+      setDiaries(updatedDiaries);
+      setEditingIndex(null);
+    } else {
+      // 新規追加
+      setDiaries([...diaries, { date: dateObject, title: newEntry.title, content: newEntry.content }]);
+    }
+    setShowPopup(false);
+    setNewEntry({ date: "", title: "", content: "" });
+  };
+
+  const handleEditEntry = (index:any) => {
+    //const entryToEdit = diaries[index];
+    //setNewEntry(entryToEdit);
+    setEditingIndex(index);
+    setShowPopup(true);
+  };
+
   return (
     <div className="diary-app">
       <div className="diary-entries">
         {diaries.map((diary, index) => (
-          <DiaryEntry key={index} {...diary} />
+          <div key={index}>
+            <DiaryEntry {...diary} />
+            <button onClick={() => handleEditEntry(index)}>✏️</button>
+          </div>
         ))}
       </div>
+      {showPopup && (
+        <div className="popup">
+          <input type="date" name="date" value={newEntry.date} onChange={handleInputChange} />
+          <input type="text" name="title" placeholder="Title" value={newEntry.title} onChange={handleInputChange} />
+          <textarea name="content" placeholder="Content" value={newEntry.content} onChange={handleInputChange} />
+          <button onClick={handleAddEntry}>{editingIndex !== null ? "Update Entry" : "Add Entry"}</button>
+        </div>
+      )}
+      <button onClick={() => setShowPopup(true)}>+</button>
     </div>
   );
 }
