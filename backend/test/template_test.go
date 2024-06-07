9e1a3aaa6a697d395a2b57867db705aed95ee77c
aster <137767097+aster-void@users.noreply.github.com>
2024-06-07 15:32:17 +0900

    fixed test error

--- backend/test/template_test.go
+++ backend/test/template_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestTemplate(t *testing.T) {
 	got := 1 + 1
-	if got != 3 {
-		t.Errorf("Test fail: Expected 1 + 1 to be equal to 3, got %d", got)
+	if got != 2 {
+		t.Errorf("Test fail: Expected 1 + 1 to be equal to 2, got %d", got)
 	}
 }
