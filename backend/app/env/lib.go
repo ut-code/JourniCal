4efde42e89cb4e6b63b983349d79e45629b7cd80
aster <137767097+aster-void@users.noreply.github.com>
2024-07-03 13:57:51 +0900

    [feat] add env package for loading env variables

--- backend/app/env/lib.go
+++ backend/app/env/lib.go
@@ -1,11 +1,22 @@
 package env
 
-import "github.com/joho/godotenv"
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 var USE_TOKEN_JSON = false
+var STATIC_USER = false
 
 // WIP
 
 func init() {
 	godotenv.Load()
+	if os.Getenv("USE_TOKEN_JSON") == "true" {
+		USE_TOKEN_JSON = true
+	}
+	if os.Getenv("STATIC_USER") == "true" {
+		STATIC_USER = true
+	}
 }
