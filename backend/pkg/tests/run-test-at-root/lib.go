9d775bc9aafc280f76f34c5349608ff0a48d207b
aster <137767097+aster-void@users.noreply.github.com>
2024-07-04 11:45:54 +0900

    [doc] add a comment in pkg/tests/run-test-at-root/lib.go

--- backend/pkg/tests/run-test-at-root/lib.go
+++ backend/pkg/tests/run-test-at-root/lib.go
@@ -8,6 +8,14 @@ import (
 	"runtime"
 )
 
+/*
+go tests run at the directory where the test is located at rather than working directory.
+this package allow to run tests at /backend instead of /backend/wherever/the/test/file/is/at,
+s.t. it can actually read files such as credentials.json without copying them all around.
+ref:
+https://intellij-support.jetbrains.com/hc/en-us/community/posts/360009685279-Go-test-working-directory-keeps-changing-to-dir-of-the-test-file-instead-of-value-in-template
+*/
+
 func init() {
 	_, filename, _, _ := runtime.Caller(0)
 	dir := path.Join(path.Dir(filename), "../../..")
