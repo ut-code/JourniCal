8da96a4777496fca0d31de5c32a6c4b764dc81ee
aster <137767097+aster-void@users.noreply.github.com>
2024-07-24 01:03:01 +0900

    fix typo

--- backend/app/router/calendar.go
+++ backend/app/router/calendar.go
@@ -15,7 +15,7 @@ func Calendar(g *echo.Group, db *gorm.DB) {
 	g.GET("/get-events-in-range/:start_unix/:end_unix", func(c echo.Context) error {
 		srv, err := calendar.SrvFromContext(db, c)
 		if err != nil {
-			return c.String(500, "Couldn't start server")
+			return c.String(500, "Couldn't start service")
 		}
 		start, err := strconv.Atoi(c.Param("start_unix"))
 		if err != nil {
