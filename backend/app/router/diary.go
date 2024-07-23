737e863d16a7fdfdb9bdcb29eb6acbb73a4b1b46
aster <137767097+aster-void@users.noreply.github.com>
2024-07-23 14:28:52 +0900

    [refac] rewrite diary funcs

--- backend/app/router/diary.go
+++ backend/app/router/diary.go
@@ -1,7 +1,11 @@
 package router
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/ut-code/JourniCal/backend/app/diary"
+	"github.com/ut-code/JourniCal/backend/app/user"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -9,42 +13,88 @@ import (
 
 func Diary(g *echo.Group, db *gorm.DB) {
 	g.GET("/", func(c echo.Context) error {
-		status, json, err := diary.GetAllOfUser(c, db)
+		u, err := user.FromEchoContext(db, c)
 		if err != nil {
-			return c.JSON(status, echo.Map{"error": err.Error()})
+			return c.String(http.StatusBadRequest, "authentication error")
 		}
-		return c.JSON(status, json)
+		json, err := diary.GetAll(db, u)
+		if err != nil {
+			return c.String(500, err.Error())
+		}
+		return c.JSON(200, json)
 	})
 
 	g.GET("/:id", func(c echo.Context) error {
-		status, diary, err := diary.Get(c, db)
-		if err != nil {
-			return c.JSON(status, echo.Map{"error": err.Error()})
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil || id < 0 {
+			return c.String(http.StatusBadRequest, "invalid id format")
 		}
-		return c.JSON(status, diary)
+		u, err := user.FromEchoContext(db, c)
+		if err != nil {
+			return c.String(http.StatusUnauthorized, "authentication error")
+		}
+		diary, err := diary.Get(db, uint(id), u)
+		if err != nil {
+			// TODO: fix db error included in here
+			return c.String(http.StatusForbidden, err.Error())
+		}
+		return c.JSON(200, diary)
 	})
 
+	// CREATE
 	g.POST("/", func(c echo.Context) error {
-		status, diary, err := diary.Create(c, db)
-		if err != nil {
-			return c.JSON(status, echo.Map{"error": err.Error()})
+		d := new(diary.Diary)
+		// UNTESTED: I'm not sure how this works or how this should be done. Test this function.
+		if err := c.Bind(d); err != nil {
+			return c.String(http.StatusBadRequest, "failed to bind diary data")
 		}
-		return c.JSON(status, diary)
+		u, err := user.FromEchoContext(db, c)
+		if err != nil {
+			return c.String(http.StatusUnauthorized, "invalid user. consider creating new one")
+		}
+		diary, err := diary.Create(db, d, u)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		return c.JSON(http.StatusCreated, diary)
 	})
 
+	// UPDATE
 	g.PUT("/:id", func(c echo.Context) error {
-		status, diary, err := diary.Update(c, db)
-		if err != nil {
-			return c.JSON(status, echo.Map{"error": err.Error()})
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil || id < 0 {
+			return c.String(http.StatusBadRequest, "Invalid formating of id or negative value")
 		}
-		return c.JSON(status, diary)
+		u, err := user.FromEchoContext(db, c)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "Invalid user. create new one")
+		}
+
+		var newDiary diary.Diary
+		if err := c.Bind(&newDiary); err != nil {
+			return c.String(http.StatusBadRequest, "Failed to bind diary")
+		}
+		diary, err := diary.Update(db, uint(id), &newDiary, u)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Failed to update")
+		}
+		return c.JSON(http.StatusAccepted, diary)
 	})
 
+	// DELETE
 	g.DELETE("/:id", func(c echo.Context) error {
-		status, err := diary.Delete(c, db)
+		u, err := user.FromEchoContext(db, c)
 		if err != nil {
-			return c.JSON(status, echo.Map{"error": err.Error()})
+			return c.String(http.StatusUnauthorized, "Authentication error: user not found")
 		}
-		return c.NoContent(status)
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil || id < 0 {
+			return c.String(http.StatusBadRequest, "Invalid diary ID")
+		}
+		err = diary.Delete(db, uint(id), u)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Failed to delete")
+		}
+		return c.NoContent(http.StatusAccepted)
 	})
 }
