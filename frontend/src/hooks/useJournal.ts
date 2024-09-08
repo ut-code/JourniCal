21dcdc56f9289115cc1e43849d67c5f97835f2e2
brdgb <bridgebhot@gmail.com>
2024-09-08 23:42:13 +0900

    journalの無限スクロール機能

--- frontend/src/hooks/useJournal.ts
+++ frontend/src/hooks/useJournal.ts
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { Journal } from "../types/types";
+import { add } from "date-fns";
 
 const API_ENDPOINT = "http://localhost:3000";
 
@@ -9,16 +10,25 @@ export default function useJournal() {
   const [error, setError] = useState<Error | null>(null);
 
   const fetchEntries = useCallback(async () => {
+    const today = new Date(new Date("2024-06-12").toDateString());
     setIsLoading(true);
     setError(null);
+    const startUnixTime = Math.floor(add(today, { days: -4 }).getTime() / 1000);
+    const endUnixTime = Math.floor(add(today, { days: 4 }).getTime() / 1000);
 
     try {
-      const response = await fetch(`${API_ENDPOINT}/api/journals/`);
+      const response = await fetch(
+        `${API_ENDPOINT}/api/journals/in-range/${startUnixTime}/${endUnixTime}`,
+      );
       if (!response.ok) {
         throw new Error("Failed to fetch journal entries");
       }
-      const data = await response.json();
-      setJournals(data);
+      const data: Journal[] = await response.json();
+      const sortedData = data.sort(
+        (data1, data2) =>
+          new Date(data1.date).getTime() - new Date(data2.date).getTime(),
+      );
+      setJournals(sortedData);
     } catch (error) {
       if (error instanceof Error) {
         setError(error);
@@ -32,5 +42,66 @@ export default function useJournal() {
   useEffect(() => {
     fetchEntries();
   }, [fetchEntries]);
-  return { journals, isLoading, error, fetchEntries };
+
+  const fetchMoreEntriesAfter = useCallback(async (bottomDate: Date) => {
+    setError(null);
+    const startUnixTime = Math.floor(
+      add(bottomDate, { days: 1 }).getTime() / 1000,
+    );
+    const endUnixTime = Math.floor(
+      add(bottomDate, { days: 4 }).getTime() / 1000,
+    );
+
+    try {
+      const response = await fetch(
+        `${API_ENDPOINT}/api/journals/in-range/${startUnixTime}/${endUnixTime}`,
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch journal entries");
+      }
+      const data = await response.json();
+      setJournals((prev) =>
+        prev === null ? data : data === null ? prev : [...prev, ...data],
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error);
+        setJournals(null);
+      }
+    }
+  }, []);
+
+  const fetchMoreEntriesBefore = useCallback(async (topDate: Date) => {
+    setError(null);
+    const startUnixTime = Math.floor(
+      add(topDate, { days: -3 }).getTime() / 1000,
+    );
+    const endUnixTime = Math.floor(topDate.getTime() / 1000);
+
+    try {
+      const response = await fetch(
+        `${API_ENDPOINT}/api/journals/in-range/${startUnixTime}/${endUnixTime}`,
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch journal entries");
+      }
+      const data = await response.json();
+      setJournals((prev) =>
+        prev === null ? data : data === null ? prev : [...data, ...prev],
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error);
+        setJournals(null);
+      }
+    }
+  }, []);
+  return {
+    journals,
+    isLoading,
+    error,
+    fetchEntries,
+    fetchMoreEntriesAfter,
+    fetchMoreEntriesBefore,
+  };
 }
--- frontend/src/pages/journal.tsx
+++ frontend/src/pages/journal.tsx
@@ -1,10 +1,86 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import JournalEntry from "../components/JournalEntry";
 import useJournal from "../hooks/useJournal";
+import { add } from "date-fns";
 
 function Journal() {
-  const { journals, isLoading, error } = useJournal();
+  const {
+    journals,
+    isLoading,
+    error,
+    fetchMoreEntriesAfter,
+    fetchMoreEntriesBefore,
+  } = useJournal();
+  const [topDate, setTopDate] = useState<Date>(
+    add(new Date(new Date("2024-06-12").toDateString()), { days: -4 }),
+  );
+  const [bottomDate, setBottomDate] = useState<Date>(
+    add(new Date(new Date("2024-06-12").toDateString()), { days: 4 }),
+  );
+  const topTargetRef = useRef<HTMLDivElement>(null);
+  const bottomTargetRef = useRef<HTMLDivElement>(null);
+
+  const topScrollObserver = useCallback(
+    () =>
+      new IntersectionObserver(
+        async (entries) => {
+          if (entries[0].isIntersecting) {
+            await fetchMoreEntriesBefore(topDate);
+            setTopDate((prev) => add(prev, { days: -3 }));
+          }
+        },
+        {
+          root: null,
+          rootMargin: "300px",
+          threshold: 0.01,
+        },
+      ),
+    [fetchMoreEntriesBefore, topDate],
+  );
+
+  const bottomScrollObserver = useCallback(
+    () =>
+      new IntersectionObserver(
+        async (entries) => {
+          if (entries[0].isIntersecting) {
+            await fetchMoreEntriesAfter(bottomDate);
+            setBottomDate((prev) => add(prev, { days: 3 }));
+          }
+        },
+        {
+          root: null,
+          rootMargin: "300px",
+          threshold: 0.01,
+        },
+      ),
+    [fetchMoreEntriesAfter, bottomDate],
+  );
+
+  useEffect(() => {
+    const topTarget = topTargetRef.current;
+    if (topTarget) {
+      const topObserver = topScrollObserver();
+      topObserver.observe(topTarget);
+      return () => {
+        topObserver.unobserve(topTarget);
+      };
+    }
+  }, [topScrollObserver, topTargetRef]);
+
+  useEffect(() => {
+    const bottomTarget = bottomTargetRef.current;
+    if (bottomTarget) {
+      const bottomObserver = bottomScrollObserver();
+      bottomObserver.observe(bottomTarget);
+      return () => {
+        bottomObserver.unobserve(bottomTarget);
+      };
+    }
+  }, [bottomScrollObserver, bottomTargetRef]);
+
   return (
     <div className="journal-app">
+      <div ref={topTargetRef} />
       {isLoading ? (
         <div>Loading...</div>
       ) : error ? (
@@ -16,6 +92,7 @@ function Journal() {
           ))}
         </div>
       )}
+      <div ref={bottomTargetRef} />
     </div>
   );
 }
