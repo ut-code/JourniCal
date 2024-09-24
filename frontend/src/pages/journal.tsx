8cf3e77c3b1cdb18b23bdb2427ff1ea11bb82994
Hideaki HASHIMOTO <104970845+brdgb@users.noreply.github.com>
2024-09-24 20:13:42 +0900

    journalの無限スクロール機能 (#69)
    * journalの無限スクロール機能
    
    * journalにtop barを設置
    
    * 抜け漏れなく日付順に取得できる
    
    * lint fmt

--- frontend/src/pages/journal.tsx
+++ frontend/src/pages/journal.tsx
@@ -1,10 +1,89 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import JournalEntry from "../components/JournalEntry";
 import useJournal from "../hooks/useJournal";
+import { add } from "date-fns";
+import TopBar from "../components/TopBar";
 
 function Journal() {
-  const { journals, isLoading, error } = useJournal();
+  const {
+    journals,
+    isLoading,
+    error,
+    fetchMoreEntriesAfter,
+    fetchMoreEntriesBefore,
+  } = useJournal();
+  const [baseDate, setBaseDate] = useState(new Date());
+  const [topDate, setTopDate] = useState<Date>(
+    new Date(new Date("2024-09-17").toDateString()),
+  );
+  const [bottomDate, setBottomDate] = useState<Date>(
+    new Date(new Date("2024-09-17").toDateString()),
+  );
+  const topTargetRef = useRef<HTMLDivElement>(null);
+  const bottomTargetRef = useRef<HTMLDivElement>(null);
+
+  const topScrollObserver = useCallback(
+    () =>
+      new IntersectionObserver(
+        async (entries) => {
+          if (entries[0].isIntersecting) {
+            await fetchMoreEntriesBefore(topDate);
+            setTopDate((prev) => add(prev, { days: -4 }));
+          }
+        },
+        {
+          root: null,
+          rootMargin: "200px",
+          threshold: 0.01,
+        },
+      ),
+    [fetchMoreEntriesBefore, topDate],
+  );
+
+  const bottomScrollObserver = useCallback(
+    () =>
+      new IntersectionObserver(
+        async (entries) => {
+          if (entries[0].isIntersecting) {
+            await fetchMoreEntriesAfter(bottomDate);
+            setBottomDate((prev) => add(prev, { days: 4 }));
+          }
+        },
+        {
+          root: null,
+          rootMargin: "200px",
+          threshold: 0.01,
+        },
+      ),
+    [fetchMoreEntriesAfter, bottomDate],
+  );
+
+  useEffect(() => {
+    const topTarget = topTargetRef.current;
+    if (topTarget) {
+      const topObserver = topScrollObserver();
+      topObserver.observe(topTarget);
+      return () => {
+        topObserver.unobserve(topTarget);
+      };
+    }
+  }, [topScrollObserver, topTargetRef]);
+
+  useEffect(() => {
+    const bottomTarget = bottomTargetRef.current;
+    if (bottomTarget) {
+      const bottomObserver = bottomScrollObserver();
+      bottomObserver.observe(bottomTarget);
+      return () => {
+        bottomObserver.unobserve(bottomTarget);
+      };
+    }
+  }, [bottomScrollObserver, bottomTargetRef]);
+
   return (
     <div className="journal-app">
+      <TopBar baseDate={baseDate} setBaseDate={setBaseDate} />
+      <div ref={topTargetRef} style={{ marginTop: "20%" }} />
       {isLoading ? (
         <div>Loading...</div>
       ) : error ? (
@@ -16,6 +95,7 @@ function Journal() {
           ))}
         </div>
       )}
+      <div ref={bottomTargetRef} />
     </div>
   );
 }
