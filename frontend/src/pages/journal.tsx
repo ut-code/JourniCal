68ce14746eeb6c9de9a8697bef8e456aff6a2c65
brdgb <bridgebhot@gmail.com>
2024-11-12 22:43:35 +0900

    fmt

--- frontend/src/pages/journal.tsx
+++ frontend/src/pages/journal.tsx
@@ -38,8 +38,8 @@ function Journal() {
         const baseEvent = await fetchScheduleByEventId(baseEventId);
         if (baseEvent && baseEvent.start) {
           setBaseDate(baseEvent.start);
-          setTopDate(add(baseEvent.start, {days: -4}));
-          setBottomDate(add(baseEvent.start, {days: 4}));
+          setTopDate(add(baseEvent.start, { days: -4 }));
+          setBottomDate(add(baseEvent.start, { days: 4 }));
         }
       }
     })();
